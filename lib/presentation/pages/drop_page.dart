e240e5fbf5495092e6f02cbb4c2982e80c8cb76a
AlyssonTrugilio <alyson.silvatrugilio121@gmail.com>
2024-08-12 12:02:44 -0300

    feat: implementado drop de arquivos utilizando flutter_dropzone

--- lib/presentation/pages/drop_page.dart
+++ lib/presentation/pages/drop_page.dart
@@ -1,10 +1,31 @@
 import 'dart:developer';
 
+import 'package:flutter_dropzone/flutter_dropzone.dart';
 import 'package:flutter/material.dart';
-import 'package:zip_and_unzip_files/core/core.dart';
 
-class DropPage extends StatelessWidget {
+import '../../core/core.dart';
+
+class DropPage extends StatefulWidget {
   const DropPage({super.key});
+
+  @override
+  State<DropPage> createState() => _DropPageState();
+}
+
+class _DropPageState extends State<DropPage> {
+  late DropzoneViewController controller;
+  String message = 'Solte o arquivo aqui';
+  bool highlighted = false;
+  final Color colors = Colors.transparent;
+  bool isFileSelected = false;
+  String url = '';
+
+  void displayImage(dynamic filePicked) async {
+    url = await controller.createFileUrl(filePicked);
+    setState(() {
+      isFileSelected = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,22 +38,81 @@ class DropPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () => log('Clicou no "Selecionar arquivo"'),
-              child: Container(
-                width: 400,
-                height: 400,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: const Color(0xFF555151),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () async {
+                    final event = await controller.pickFiles();
+                    displayImage(event.first);
+                  },
+                  child: Container(
+                    width: 400,
+                    height: 400,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: highlighted ? Colors.red : colors,
+                    ),
+                    child: Stack(
+                      children: [
+                        DropzoneView(
+                          onCreated: (ctrl) => controller = ctrl,
+                          cursor: CursorType.grab,
+                          onHover: () {
+                            setState(() => highlighted = true);
+                          },
+                          onLeave: () {
+                            setState(() => highlighted = false);
+                          },
+                          onDrop: (ev) async {
+                            log('Zone 1 drop: ${ev.name}');
+                            setState(() {
+                              message = '${ev.name} dropped';
+                              highlighted = false;
+                              colors;
+                            });
+                            final bytes = await controller.getFileData(ev);
+                            displayImage(ev);
+                            log('${bytes.sublist(0, 20)}');
+                          },
+                        ),
+                        Center(
+                          child: Text(
+                            message,
+                            style: const TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-                child: const Center(
-                  child: Text('Selecionar arquivo'),
-                ),
-              ),
+                const SizedBox(height: 10),
+                (isFileSelected)
+                    ? Image.network(
+                        url,
+                        fit: BoxFit.cover,
+                        width: 400,
+                        height: 300,
+                      )
+                    : const SizedBox.shrink()
+              ],
             ),
+            const SizedBox(height: 50),
+            SizedBox(
+              height: 70,
+              width: 120,
+              child: FilledButton(
+                onPressed: () => setState(() {
+                  message = 'Arraste o arquivo';
+
+                  isFileSelected = false;
+                }),
+                child: const Text('Reset'),
+              ),
+            )
           ],
         ),
       ),
